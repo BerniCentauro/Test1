091883092495bdebf0b43e2448bec7cd83819b25
Berni Hidalgo <berni@centaurosolutions.com>
2016-08-16 14:01:55 -0600

    Dice hola todos

--- WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Location = new System.Drawing.Point(104, 84);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(65, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Berni Hidalgo";
+            this.label1.Text = "Hola Mundo";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +69,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -21,5 +21,10 @@ namespace WindowsFormsApp
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hola todos";
+        }
     }
 }
